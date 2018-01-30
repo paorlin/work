b70478d85802a3d9c3dace8c111d7d2dd66756e9
Patrick Ortmann <patrick.ortmann07@gmail.com>
2018-01-30 17:47:08 -0500

    tarea

---
+++ README.md
@@ -0,0 +1,1 @@
+Tis programs replaces numbers divisible by 3 with a Fizz, numbers divisible by a 5 with Buzz and if they are divisible by both, a FizzBuzz
---
+++ script.rb
@@ -0,0 +1,13 @@
+(1..100).each { |n|
+  if n%3==0 && n%5==0
+    puts  n = 'FizzBuzz'
+  else if n%5==0
+    puts n = 'Buzz'
+  else if n%3==0
+    puts n = 'Fizz'
+  else
+    puts n
+  end
+  end
+  end
+}
